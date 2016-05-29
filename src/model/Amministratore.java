68793f3044cdedc35d196baf0a24182422ebdb66
Meyke <michele.tedesco487@virgilio.it>
2016-05-29 15:02:39 +0200

    provato commit da eclipse

--- src/model/Amministratore.java
+++ src/model/Amministratore.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 
 
 @Entity
-public class Amministratore {
+public class Amministratore { //aggiungere vari fetch
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
